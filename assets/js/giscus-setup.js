9a436f4d8df0118f4ab40720e4452b1a6247e5c9
ddecosmo-dev <ddecosmo@andrew.cmu.edu>
2026-04-02 21:40:50 +0000

    Deploying to gh-pages from @ ddecosmo-dev/ddecosmo.github.io@1b27d570e31a42a03f2d03a1ee6c3bd9335dd8a9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ddecosmo-dev/portfolio",
+    "data-repo": "ddecosmo-dev/ddecosmo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
